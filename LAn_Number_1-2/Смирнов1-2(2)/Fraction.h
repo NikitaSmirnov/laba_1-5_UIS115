4508823b5c9dccd47a50d31e1ca428a690cad1fe
NikitaSmirnov <smirnov.nikita.v@mail.ru>
2021-06-11 14:26:16 +0300

    лаба 1-2(переделанно)

---
+++ LAn_Number_1-2/Смирнов1-2(2)/Fraction.h
@@ -0,0 +1,44 @@
+#pragma once
+#include"Pair.h"
+/**
+* \brief Класс дробь, наследованный от Pair
+*/
+class Fraction : public Pair {
+private:
+	double whole, fractional;
+public:
+	/**
+	* \brief Конструктор по умолчанию
+	*/
+	explicit Fraction() = default;
+
+	/**
+	* \brief Конструктор с параметрами
+	*/
+	explicit Fraction(double whole, double fractional);
+
+	/**
+	* \brief Деструктор по умолчанию
+	*/
+	~Fraction();
+
+	/**
+	* \brief Перегрузка оператора <
+	*/
+	bool operator <(const Fraction& Other)const;
+
+	/**
+	* \brief Перегрузка оператора >
+	*/
+	bool operator >(const Fraction& Other)const;
+
+	/**
+	* \brief Перегрузка оператора ==
+	*/
+	bool operator ==(const Fraction& Other)const;
+	
+	/**
+	* \brief Проверка на равенство переменных double
+	*/
+	bool IsEqual(const double whole, const double fractional)const;
+};
